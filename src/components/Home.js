f8a75c976117b5e23b96f7aa882cfcbdbf867c09
Muhammad wasi <wasiarain819@gmail.com>
2021-05-22 13:30:13 +0500

    Add: Left Side

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,75 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import LeftSide from "./LeftSide";
+import Main from "./Main";
+import RightSide from "./RightSide";
 
 const Home = () => {
-    return (
-       <div>Home</div>
-    )
-}
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry -</a>
+        </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business
+          moving.
+        </p>
+      </Section>
+      <Layout>
+          <LeftSide/>
+          <RightSide />
+          <Main/>
+      </Layout>
+    </Container>
+  );
+};
 
+const Container = styled.div`
+  padding: 52px;
+  max-width: 100%;
+`;
+const Content = styled.div`
+  max-width: 1128px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Section = styled.section`
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  row-gap: 25px;
+  /* grid-template-rows: auto; */
+  margin: 25px;
+  @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+      padding: 0 5px;
+  }
+`;
 export default Home;
---
+++ src/components/RightSide.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const RightSide = (props) => {
+  return <Container>Right side</Container>;
+};
+
+const Container = styled.div`
+  grid-area: rightside;
+`;
+
+export default RightSide;
